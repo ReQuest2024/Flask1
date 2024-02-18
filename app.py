f94e6d237c3bf161b9d3756e2046af8b0dcd1825
ReQuest2024 <>
2024-02-18 18:43:57 +0300

    day5 task 1-3

--- app.py
+++ app.py
@@ -63,28 +63,74 @@ def handle_exception(e):
     return jsonify({"message": e.description}), e.code
 
 
+## Authors
+# GET на url: /authors              # получить список авторов
+@app.get("/authors")
+def get_all_authors():
+    authors_lst = db.session.query(AuthorModel)   
+    authors_lst_dct = []
+    for author in authors_lst:
+        authors_lst_dct.append(author.to_dict())
+    return jsonify(authors_lst_dct), 200
+
+
+# GET на url: /authors/<int:author_id>     # получить автора с id = <int:author_id>
+@app.get("/authors/<int:author_id>")
+def get_author_by_id(author_id):
+    author = db.session.get(AuthorModel, author_id)   
+    if author:
+        return jsonify(author.to_dict()), 200
+    abort(404, f"Author with author_id = {author_id} not found")
+
+
+# POST на url: /authors             # создать нового автора
 @app.post("/authors")
 def create_author():
     author_data = request.json
+    authors_name = db.session.query(AuthorModel).filter_by(name = author_data.get("name", "noname"))
+    if len(authors_name.all()):
+        abort(500, f"Author with name = '{author_data.get('name', 'noname')}' already exists")
     author = AuthorModel(author_data.get("name", "noname")) 
     db.session.add(author)
     db.session.commit()
     return jsonify(author.to_dict()), 201
 
 
-@app.post("/authors/<int:author_id>/quotes")
-def create_quote_to_author(author_id):
-    author = AuthorModel.query.get(author_id)
-    data = request.json
-    new_quote = QuoteModel(author, data.get("text", "blank")) 
-    db.session.add(new_quote)
-    db.session.commit()
-    return new_quote.to_dict(), 201
+# PUT на url: /authors/<int:id>     # обновить автора с id = <int:id>
+@app.put("/authors/<int:author_id>")
+def edit_author_by_id(author_id):
+    new_data = request.json
+    author = db.session.get(AuthorModel, author_id)   
+    if not author:
+        abort(404, f"Author with author_id = {author_id} not found")
+    authors_name = db.session.query(AuthorModel).filter_by(name = new_data.get("name", "noname"))
+    if len(authors_name.all()):
+        abort(500, f"Author with name = '{new_data.get('name', 'noname')}' already exists")
+    for key, value in new_data.items():
+        setattr(author, key, value)
+    try:
+        db.session.commit()
+        return jsonify(author.to_dict()), 200
+    except:
+        abort(500)
 
 
-# GET на url: /quotes/
+# DELETE на url: /authors/<int:id>  # удалить автора с id = <int:id>
+@app.delete("/authors/<int:author_id>")
+def delete_author_by_id(author_id):
+    quote = db.session.get(AuthorModel, author_id)
+    if quote:
+        db.session.delete(quote)
+        db.session.commit()
+        return {"message": f"Author with author_id = {author_id} deleted."}, 200
+    abort(404, f"Author with author_id = {author_id} not found")
+
+
+
+## Quotes
+# GET на url: /quotes/              # все цитаты всех авторов
 @app.route("/quotes")
-def get_quotes():
+def get_all_quotes():
     quotes_lst = db.session.query(QuoteModel)   
     quotes_lst_dct = []
     for quote in quotes_lst:
@@ -92,67 +138,65 @@ def get_quotes():
     return jsonify(quotes_lst_dct), 200
 
 
-# GET на url: /quotes/<int:id>
-@app.get("/quotes/<int:id>")
-def get_quote_id(id):
-    quote = db.session.get(QuoteModel, id)   
+# GET на url: /quotes/<int:quote_id>      # получить цитату с quote_id = <int:quote_id>
+@app.get("/quotes/<int:quote_id>")
+def get_quote_by_id(quote_id):
+    quote = db.session.get(QuoteModel, quote_id)   
     if not quote is None:
         return jsonify(quote.to_dict()), 200
-    abort(404, f"Quote id = {id} not found")
+    abort(404, f"Quote with quote_id = {quote_id} not found")
 
 
-# POST на url: /quotes/
-@app.post("/quotes")
-def create_quote():
-    new_quote = request.json
-    if new_quote.get("author") and new_quote.get("text"):
-        new_row = QuoteModel(new_quote.get("author"), new_quote.get("text"), new_quote.get("rating"))
-        db.session.add(new_row)
-        db.session.commit()
-        print(new_row.to_dict())
-        return jsonify(new_row.to_dict()), 200
-    abort(400, "NOT NULL constraint failed")
+# GET на url: /authors/<int:id>/quotes      # получить все цитаты автора с quote_id = <int:quote_id>
+@app.get("/authors/<int:author_id>/quotes")
+def get_quote_by_author(author_id):
+    quotes_lst = db.session.query(QuoteModel).filter_by(author_id = author_id)   
+    quotes_lst_dct = []
+    for quote in quotes_lst:
+        quotes_lst_dct.append(quote.to_dict())
+    return jsonify(quotes_lst_dct), 200
 
 
-# PUT на url: /quotes/<int:id>
-@app.put("/quotes/<int:id>")
-def edit_quote(id):
+# POST на url: /authors/<int:id>/quotes      # создать цитату для автора с quote_id = <int:quote_id>
+@app.post("/authors/<int:author_id>/quotes")
+def create_quote_to_author(author_id):
+    author = db.session.get(AuthorModel, author_id)   
+    if not author:
+        abort(404, f"Author with author_id = {author_id} not found")
+    data = request.json
+    new_quote = QuoteModel(author, data.get("text", "blank")) 
+    db.session.add(new_quote)
+    db.session.commit()
+    return new_quote.to_dict(), 201
+
+
+# PUT на url: /quotes/<int:quote_id>     # обновить цитату с quote_id = <int:quote_id>
+@app.put("/quotes/<int:quote_id>")
+def edit_quote_by_id(quote_id):
     new_data = request.json
-    quote = db.session.get(QuoteModel, id) 
-    if not quote is None:
-        if not new_data.get("author") is None:
-            quote.author = new_data.get("author")
-        if not new_data.get("text") is None:
-            quote.text = new_data.get("text")
-        if not new_data.get("rating") is None and 0 < new_data.get("rating") < 6:
-            quote.rating = new_data.get("rating")
+    quote = db.session.get(QuoteModel, quote_id) 
+    if not quote:
+        abort(404, f"Quthor with quote_id = {quote_id} not found")
+    for key, value in new_data.items():
+        setattr(quote, key, value)
+    try:
         db.session.commit()
         return jsonify(quote.to_dict()), 200
-    abort(404, f"Quote id = {id} not found")
+    except:
+        abort(500)
 
 
-# DELETE на url: /quotes/<int:id>
-@app.delete("/quotes/<int:id>")
-def delete(id):
-    quote = db.session.get(QuoteModel, id)
-    if not quote is None:
+# DELETE на url: /quotes/<int:quote_id>  # удалить цитату с quote_id = <int:quote_id>
+@app.delete("/quotes/<int:quote_id>")
+def delete_quote_by_id(quote_id):
+    quote = db.session.get(QuoteModel, quote_id)
+    if quote:
         db.session.delete(quote)
         db.session.commit()
-        return {"message": f"Row with id={id} deleted."}, 200
-    abort(404, f"Quote id = {id} not found")
-    
+        return {"message": f"Quote with quote_id = {quote_id} deleted."}, 200
+    abort(404, f"Quote with quote_id = {quote_id} not found")
 
-# GET filter
-@app.get("/quotes/filter")
-def get_quote_by_filter():
-    kwargs = request.args
-    quotes = QuoteModel.query.filter_by(**kwargs).all()
-    quotes_lst = []
-    if quotes:
-        for quote in quotes:
-            quotes_lst.append(quote.to_dict())
-        return jsonify(quotes_lst), 200
-    return jsonify([]), 200
+
 
 
 
